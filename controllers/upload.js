589d150eca9c558577f17e0d45a379f153b36ee2
Nirmal Almara <rkumarnirmal@gmail.com>
2016-11-18 20:59:49 +0530

    upload book with covers

--- controllers/upload.js
+++ controllers/upload.js
@@ -4,6 +4,7 @@ var Book = require('../models/book')
 var router = express.Router()
 var path = require('path')
 var uploadDir = path.join(__dirname, '/../uploads/')
+var uploadDir2 = path.join(__dirname, '/..')
 var imageDir = path.join(__dirname, '/../uploads/images/')
 var exec = require('child_process').exec
 
@@ -16,6 +17,62 @@ router.post('/api/upload', function (req, res, next) {
 
   form.parse(req, (err, fields, files) => {
     if (err) return res.status(500).json({ error: err })
+
+    Book.find({email: email}, function (err, content) {
+      if (err) return console.error(err)
+
+      if (Array.isArray(files.userBooks)) {
+        files.userBooks.forEach(function (item, index) {
+          var bookPath = item.path.split('LibreRead')[1]
+          console.log(bookPath)
+          if (content.length) {
+            content[0].list.push(bookPath)
+            content[0].save(function (err) {
+              if (err) return console.error(err)
+              console.log('books added successfully')
+              return getCover(bookPath)
+            })
+          } else {
+            var book = new Book({email: email, list: [bookPath]})
+            book.save(function (err, content) {
+              if (err) return console.error(err)
+              console.log(content)
+              console.log('books added successfully')
+              return getCover(bookPath)
+            })
+          }
+        })
+      } else {
+        var bookPath = files.userBooks.path.split('LibreRead')[1]
+        console.log(bookPath)
+        console.log(content)
+        if (content.length) {
+          content[0].list.push(bookPath)
+          content[0].save(function (err) {
+            if (err) return console.error(err)
+            console.log('books added successfully')
+            return getCover(bookPath)
+          })
+        } else {
+          var book = new Book({email: email, list: [bookPath]})
+          book.save(function (err, content) {
+            if (err) return console.error(err)
+            console.log(content)
+            console.log('books added successfully')
+            return getCover(bookPath)
+          })
+        }
+      }
+    })
+
+    function getCover (bookPath) {
+      var cmd = 'pdfimages -p -png -f 1 -l 2 ' + uploadDir2 + bookPath + ' ' + imageDir + bookPath.split('/uploads/').pop()
+      console.log(cmd)
+      exec(cmd, function (err, stdout, stderr) {
+        if (err) return console.error(err)
+        console.log(stdout)
+      })
+    }
 
     res.redirect('/')
   })
